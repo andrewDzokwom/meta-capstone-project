f898d87297e857f8fe1c100cec88308198aef337
andrewDzokwom <andrewa441@gmail.com>
2024-11-02 08:07:40 +0100

    project init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Starting point</h1>
+      </div>
+      <h1>Capstone Project</h1>
+    </>
+  )
+}
+
+export default App
